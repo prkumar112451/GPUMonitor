22eeb26fca7d5c81ecb2456633fdd8788256e007
prkumar112451 <38156723+prkumar112451@users.noreply.github.com>
2024-05-20 14:26:47 +0530

    Update gpu_monitor.py

--- gpu_monitor.py
+++ gpu_monitor.py
@@ -1,5 +1,10 @@
 import time
 import pynvml
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def monitor_gpu():
     pynvml.nvmlInit()
@@ -12,7 +17,7 @@ def monitor_gpu():
             utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
 
             timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            print(f"{timestamp} - GPU {i}: Memory Used: {mem_info.used / 1024**2:.2f} MB Memory Total: {mem_info.total / 1024**2:.2f} MB GPU Utilization: {utilization.gpu}% Memory Utilization: {utilization.memory}% -------------------------------", end="\r")
+            logger.info(f"{timestamp} - GPU {i}: Memory Used: {mem_info.used / 1024**2:.2f} MB Memory Total: {mem_info.total / 1024**2:.2f} MB GPU Utilization: {utilization.gpu}% Memory Utilization: {utilization.memory}% -------------------------------", end="\r")
 
         time.sleep(2)
 
