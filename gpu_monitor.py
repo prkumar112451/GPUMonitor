a481df180a99466b00b184803fe3b82dd6861977
prkumar112451 <38156723+prkumar112451@users.noreply.github.com>
2024-05-20 14:12:49 +0530

    Update gpu_monitor.py

--- gpu_monitor.py
+++ gpu_monitor.py
@@ -6,14 +6,13 @@ def monitor_gpu():
     device_count = pynvml.nvmlDeviceGetCount()
 
     while True:
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        print(f"{timestamp} - ", end="")
         for i in range(device_count):
             handle = pynvml.nvmlDeviceGetHandleByIndex(i)
             mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
             utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
 
-            print(f"GPU {i}: Memory Used: {mem_info.used / 1024**2:.2f} MB Memory Total: {mem_info.total / 1024**2:.2f} MB GPU Utilization: {utilization.gpu}% Memory Utilization: {utilization.memory}% -------------------------------", end="\r")
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            print(f"{timestamp} - GPU {i}: Memory Used: {mem_info.used / 1024**2:.2f} MB Memory Total: {mem_info.total / 1024**2:.2f} MB GPU Utilization: {utilization.gpu}% Memory Utilization: {utilization.memory}% -------------------------------", end="\r")
 
         time.sleep(2)
 
